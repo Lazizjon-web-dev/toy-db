59bf7f04ed8d988beece57e30c90f8e9222adda1
Lazizjon-web-dev <azizjonovlazizjon64@gmail.com>
2025-06-10 15:16:49 +0500

    feat(db): Add unit tests for Database struct initialization

--- src/db.rs
+++ src/db.rs
@@ -1,7 +1,9 @@
 use serde::{Deserialize, Serialize};
+use serde_json::from_str;
 use std::collections::HashMap;
 use std::fs;
 
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Database {
     pub value: HashMap<String, String>,
 }
@@ -15,6 +17,23 @@ impl Database {
 
     pub fn initialize_from_file(&mut self, file_path: &str) -> Result<(), String> {
         let raw = fs::read_to_string(file_path).expect("Should have been able to read the file");
-        let contents = serialze::<HashMap<String, String>>(raw);
+        let contents = from_str::<HashMap<String, String>>(&raw);
+        self.value = contents.unwrap_or_default();
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_database_initialization() {
+        let mut db = Database::new();
+        assert!(db.value.is_empty());
+        let path = "test_db.json";
+        db.initialize_from_file(path).unwrap();
+
+        assert_eq!(db.value.get("name").unwrap(), &String::from("Alan"))
     }
 }
