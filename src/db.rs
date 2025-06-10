8c8e549e1da848ffbe7f7b3e520ceb39baf0e99b
Lazizjon-web-dev <azizjonovlazizjon64@gmail.com>
2025-06-10 14:41:59 +0500

    feat(db): Added basic logic for initializing DB from a file

--- src/db.rs
+++ src/db.rs
@@ -1,7 +1,20 @@
-pub struct Database {}
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+use std::fs;
+
+pub struct Database {
+    pub value: HashMap<String, String>,
+}
 
 impl Database {
     pub fn new() -> Self {
-        Database {}
+        Database {
+            value: HashMap::new(),
+        }
+    }
+
+    pub fn initialize_from_file(&mut self, file_path: &str) -> Result<(), String> {
+        let raw = fs::read_to_string(file_path).expect("Should have been able to read the file");
+        let contents = serialze::<HashMap<String, String>>(raw);
     }
 }
